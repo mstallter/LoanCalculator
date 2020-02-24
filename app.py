67c6e945f4f61f610f8f85bf5bfa6ce9f60bcfd4
Michael Stallter <mstallter@comcast.net>
2020-02-24 10:12:06 -0600

    final commit

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        A = request.form["amt"]
-        n = request.form["payments"]
-        i = request.form["intrate"]
-        D = (((1+i)^n)-1) / (i(1+i)^n)
+        form = request.form
+        A = float(form["amt"])
+        n = float(form["payments"]) * 12
+        i = float(form["intrate"]) / 12
+        D = (((1+i)**n)-1)/(i*(1+i)**n)
         P = round(A/D, 2)
-        return 'Your loan payment is ' + P
+        print('Your loan payment is $', P, sep=' ')
+    return render_template('index.html', pageTitle='loanCalc')
 
 if __name__ == '__main__':
     app.run(debug=True)
