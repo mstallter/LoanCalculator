bc7c1f23630e2663eb3452abaec13a483f4dced3
Michael Stallter <mstallter@comcast.net>
2020-02-24 08:13:09 -0600

    still broken but better

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    #A = form[]
-    #n = form[]
-    #i = form []
-    #D = (((1+i)^n)-1) / (i(1+i)^n)
-    #P = round(A/D, 2)
-
-    return 'Your loan payment is'
+    if request.method == 'POST':
+        A = request.form["amt"]
+        n = request.form["payments"]
+        i = request.form["intrate"]
+        D = (((1+i)^n)-1) / (i(1+i)^n)
+        P = round(A/D, 2)
+        return 'Your loan payment is ' + P
 
 if __name__ == '__main__':
     app.run(debug=True)
